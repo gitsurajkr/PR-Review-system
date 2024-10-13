61ae2d5801fee46411ef2f590ed8d41e6a49dcb1
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-10-13 20:00:24 +0530

    added some Graph to visualize clear

---
+++ frontend/src/Components/Graph/PRStatusDistributionGraph.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
+
+const PRStatusDistributionGraph = () => {
+  return (
+    <div className="border-2 border-gray-400 rounded-lg shadow-lg w-full p-3 bg-white">
+      <h2 className="text-lg font-semibold text-center -mb-4">PR Status Distribution</h2>
+      <BarChart
+        xAxis={[{ scaleType: 'band', data: ['Group A', 'Group B', 'Group C'] }]}
+        series={[{ data: [4, 3, 5] }]} // Keeping only the first series
+        width={270}
+        height={300}
+        colors={['#4caf50']} // Change to a vibrant green
+      />
+      <div className="flex justify-center mt-4">
+        <span className="text-xs text-gray-600">Values: 4, 3, 5</span>
+      </div>
+    </div>
+  );
+}
+
+export default PRStatusDistributionGraph;
